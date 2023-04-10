cbba8e6983a8c6943f2048ec34a8b80746410d59
Your Name <saifi.mujeeb2@gmail.com>
2023-04-10 20:33:15 +0530

    Added: Solution of second question

---
+++ src/DistinctArray.java
@@ -0,0 +1,10 @@
+public class DistinctArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,4,7,5};
+        int arr1[] = {1,3,5,8,9};
+        for (int i =0; i< arr.length; i++){
+            if (arr[i]!=arr1[i])
+            System.out.println(arr[i]+" not equals to "+arr1[i]);
+            }
+        }
+    }
